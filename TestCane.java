c59941009cde8467f824db8db66a4007cd07d897
sasha <138666213+sashujke@users.noreply.github.com>
2024-10-01 12:03:38 +0200

    Create TestCane.java

---
+++ TestCane.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author 
+ */
+public class TestCane {
+    public static void main(String[] args) {
+        Cane cane1 = new Cane();
+        cane1.setNome("Jago");
+        cane1.setRazza("Meticcio");
+        cane1.setAnni(3);
+        cane1.EtaInAnniUmani(3);
+    }
+}
