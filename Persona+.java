778b51acbdd82cf73ef306e0111ac79a74292a08
sasha <138666213+sashujke@users.noreply.github.com>
2024-10-10 11:09:31 +0300

    Create Persona+.java

---
+++ Persona+.java
@@ -0,0 +1,42 @@
+public class Persona {
+    private String nome;
+    private String cognome;
+    private Data dataNascita;
+    
+    public Persona(){
+        this.dataNascita = new Data(1, 1, 1970);
+    };
+    
+    public Persona(String nome, String cognome, Data dataNascita){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+    }
+    
+    // getters & setters ...
+    
+    public int calcolaEta(Data dataOggi){
+        int eta = dataOggi.getAnno() - dataNascita.getAnno();
+        
+        // check compleanno
+        if (dataOggi.getMese() < dataNascita.getMese()){
+            eta--;
+        } else if (dataOggi.getMese() == dataNascita.getMese()){
+            if (dataOggi.getGiorno() < dataNascita.getGiorno()){
+                eta--;
+            }
+        } else {
+            return eta;
+        }
+                
+        return eta;
+    }
+    
+    @Override
+    public String toString(){
+        return "nome: " + nome + 
+                "\ncognome: " + cognome + 
+                "\ndata nascita: " + dataNascita.toString();
+    }
+    
+}
