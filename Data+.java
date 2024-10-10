44267e13047c990457ca3d52043ba351f5edcce4
sasha <138666213+sashujke@users.noreply.github.com>
2024-10-10 11:10:14 +0300

    Create Data+.java

---
+++ Data+.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DOCENTE
+ */
+public class Data {
+    
+    private int giorno;
+    private int mese;
+    private int anno;
+    
+    public Data(){};
+    
+    public Data(int giorno, int mese, int anno){
+        this.giorno = giorno;
+        this.mese = mese;
+        this.anno = anno;        
+    }
+    
+    public int getGiorno(){
+        return giorno;
+    }
+    
+    public int getMese(){
+        return mese;
+    }
+    
+    public int getAnno(){
+        return anno;
+    }
+    
+    // metodi setters ... METTERE
+    
+    @Override
+    public String toString(){
+        return giorno + "/" + mese + "/" + anno; 
+    }
+    
+    
+    
+}
