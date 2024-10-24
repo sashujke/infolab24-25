0d900b14fa2e8c48ea560c03b532959c51f2413c
sasha <138666213+sashujke@users.noreply.github.com>
2024-10-24 09:36:46 +0200

    Update TestPersona+.java

--- TestPersona+.java
+++ TestPersona+.java
@@ -2,6 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
+package testpersona.testpersona;
+
+/**
+ *
+ * @author SIAROU
+ */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
 /**
  *
  * @author DOCENTE
@@ -13,8 +23,12 @@ public class TestPersona {
         Persona p1 = new Persona();
         System.out.println(p1.toString());
         
-        Persona p2 = new Persona("Scorza", "RealMadrid", new Data(07, 07, 1777));
-        System.out.println(p2.toString());
+        //Persona p2 = new Persona("Scorza", "RealMadrid", new Data(07, 07, 1777));
+        //System.out.println(p2.toString());
         
+        
+    
+        Studente studente1 = new Studente ("Sasha", "Siarou", new Data(19, 04, 2006), "4i", "INF", 7.5, "57824");
+        System.out.println (studente1.toString());
     }
 }
