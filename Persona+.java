0fd5898076dd7d76a9f505e636a77fca114c6f9c
sasha <138666213+sashujke@users.noreply.github.com>
2024-10-24 09:36:34 +0200

    Update Persona+.java

--- Persona+.java
+++ Persona+.java
@@ -1,3 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testpersona.testpersona;
+
+/**
+ *
+ * @author SIAROU
+ */
 public class Persona {
     private String nome;
     private String cognome;
